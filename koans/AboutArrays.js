c6e4f58e76f261ee281d3266437e804e900e4b41
Gowri Rao <gowri.s.rao@gmail.com>
2017-12-17 15:01:44 -0800

    Fixed Arrays and Functions

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -50,7 +50,7 @@ describe("About Arrays", function() {
     expect(array.slice(0, 2)).toEqual(["peanut", "butter"]);
     expect(array.slice(2, 2)).toEqual([]);
     expect(array.slice(2, 20)).toEqual(["and", "jelly"]);
-    expect(array.slice(3, 0)).toEqual([]]);
+    expect(array.slice(3, 0)).toEqual([]);
     expect(array.slice(3, 100)).toEqual(["jelly"]);
     expect(array.slice(5, 1)).toEqual([]);
   });
